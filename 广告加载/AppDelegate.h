bb3dfb78e10910875ebed5cd1f3c867b83d004c9
残月 <632342527@qq.com>
2016-09-10 15:11:52 +0800

    Initial Commit

---
+++ 广告加载/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  广告加载
+//
+//  Created by 王奥东 on 16/9/10.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 广告加载/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  广告加载
+//
+//  Created by 王奥东 on 16/9/10.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
